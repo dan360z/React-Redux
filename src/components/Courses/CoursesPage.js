f3de385a092b086d0ea8581aa3772e0c71e6f0ac
Daniel Field <dannyf506@googlemail.com>
2020-03-24 18:46:58 +0000

    used Thunks and connect to mock api

--- src/components/Courses/CoursesPage.js
+++ src/components/Courses/CoursesPage.js
@@ -1,58 +1,60 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
+  componentDidMount() {
+    const { courses, authors } = this.props
+    if (courses.length === 0) {
+      this.props.loadCourses().catch(error => {
+        alert("Loading courses failed" + error)
+      });
     }
-  };
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
+    if (authors.length === 0) {
+      this.props.loadAuthors().catch(error => {
+        alert("Loading authors failed" + error)
+      });
+    }
   }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    //debugger;
-    this.props.actions.createCourse(this.state.course);
-  }
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input type="text" onChange={this.handleChange} value={this.state.course.title} />
-        <input type="submit" value="save" />
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
-      </form>
-    )
+        <CourseList courses={this.props.courses} />
+      </>
+    );
   }
 }
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   //debugger;
   return {
-    courses: state.courses
+    courses: state.authors.length === 0 ? [] : state.courses.map(course => {
+      return {
+        ...course,
+        authorName: state.authors.find(a => a.id === course.authorId).name
+      };
+    }),
+    authors: state.authors
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
-    //createCourse: course => dispatch(courseActions.createCourse(course)) // Dispatch is the function that notifys redux about an action.
+    loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+    loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    // createCourse: course => dispatch(courseActions.createCourse(course)) // Dispatch is the function that notifys redux about an action.
   };
 }
 
